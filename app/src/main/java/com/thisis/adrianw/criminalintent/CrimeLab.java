e79926eb91ff226abcdc20e010e4ecbb5e47df3a
ThisIsAdrianW <35401114+ThisIsAdrianW@users.noreply.github.com>
2019-01-21 16:44:06 +0100

    singleton method added

--- app/src/main/java/com/thisis/adrianw/criminalintent/CrimeLab.java
+++ app/src/main/java/com/thisis/adrianw/criminalintent/CrimeLab.java
@@ -1,4 +1,15 @@
 package com.thisis.adrianw.criminalintent;
 
+import android.content.Context;
+
 public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+    public static CrimeLab get(Context context) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+    private CrimeLab(Context context) {
+    }
 }
