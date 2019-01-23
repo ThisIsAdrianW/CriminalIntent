49d8e19d16ad90f69678609620dc052c3849946e
ThisIsAdrianW <35401114+ThisIsAdrianW@users.noreply.github.com>
2019-01-23 05:14:11 +0100

    Updating list fragment

--- app/src/main/java/com/thisis/adrianw/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/thisis/adrianw/criminalintent/CrimeListFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -36,17 +37,25 @@ public class CrimeListFragment extends Fragment {
             mCrimes = crimes;
         }
 
-        private class CrimeHolder extends RecyclerView.ViewHolder {
+        private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
             private Crime mCrime;
             public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
                 super(inflater.inflate(R.layout.list_item_crime, parent, false));
                 mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
                 mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
+                itemView.setOnClickListener(this);
             }
             public void bind(Crime crime) {
                 mCrime = crime;
                 mTitleTextView.setText(mCrime.getTitle());
                 mDateTextView.setText(mCrime.getDate().toString());
+            }
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(),
+                        mCrime.getTitle() + " klik!", Toast.LENGTH_SHORT)
+                        .show();
             }
         }
 
