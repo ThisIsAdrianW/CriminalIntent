2b99c3ba338605ab1f13bac8a04e66a6218a521f
ThisIsAdrianW <35401114+ThisIsAdrianW@users.noreply.github.com>
2019-01-23 01:55:25 +0100

    Updated Manifest

--- app/src/main/java/com/thisis/adrianw/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/thisis/adrianw/criminalintent/CrimeActivity.java
@@ -1,10 +1,6 @@
 package com.thisis.adrianw.criminalintent;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-
-import android.os.Bundle;
 
 public class CrimeActivity extends SingleFragmentActivity {
     @Override
