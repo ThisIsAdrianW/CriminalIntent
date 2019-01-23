3b8f70ad1720124e66d428a8c618e0af46a403cb
ThisIsAdrianW <35401114+ThisIsAdrianW@users.noreply.github.com>
2019-01-23 01:03:15 +0100

    Added class CrimeListFragment Added class CrimeListActivity

--- app/src/main/java/com/thisis/adrianw/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/thisis/adrianw/criminalintent/CrimeActivity.java
@@ -12,17 +12,4 @@ public class CrimeActivity extends SingleFragmentActivity {
         return new CrimeFragment();
     }
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_fragment);
-//        FragmentManager fm = getSupportFragmentManager();
-//        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-//        if (fragment == null) {
-//            fragment = new CrimeFragment();
-//            fm.beginTransaction()
-//                    .add(R.id.fragment_container, fragment)
-//                    .commit();
-//        }
-//    }
 }
---
+++ app/src/main/java/com/thisis/adrianw/criminalintent/CrimeListFragment.java
@@ -0,0 +1,6 @@
+package com.thisis.adrianw.criminalintent;
+
+import androidx.fragment.app.Fragment;
+
+public class CrimeListFragment extends Fragment {
+}
