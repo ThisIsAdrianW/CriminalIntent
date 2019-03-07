fed36e11f33fd0b48b630a6f773b2b93dc2fde47
ThisIsAdrianW <35401114+ThisIsAdrianW@users.noreply.github.com>
2019-03-07 03:11:17 +0100

    RecycleView is now working CrimeActivity is deleted

--- app/src/main/java/com/thisis/adrianw/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/thisis/adrianw/criminalintent/CrimePagerActivity.java
@@ -1,5 +1,7 @@
 package com.thisis.adrianw.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,14 +11,24 @@ import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import java.util.List;
+import java.util.UUID;
 
 public class CrimePagerActivity extends AppCompatActivity {
+    private static final String EXTRA_CRIME_ID = "com.thisis.adrianw.criminalintent.crime_id";
     private ViewPager mViewPager;
     private List<Crime> mCrimes;
+
+    public static Intent newIntent(Context packageContext, UUID crimeId) {
+        Intent intent = new Intent(packageContext, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
         mViewPager = (ViewPager) findViewById(R.id.crime_view_pager);
         mCrimes = CrimeLab.get(this).getCrimes();
         FragmentManager fragmentManager = getSupportFragmentManager();
@@ -26,6 +38,7 @@ public class CrimePagerActivity extends AppCompatActivity {
                 Crime crime = mCrimes.get(position);
                 return CrimeFragment.newInstance(crime.getId());
             }
+
             @Override
             public int getCount() {
                 return mCrimes.size();
